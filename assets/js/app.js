e6628ccd152ed342cdb0ecd3e64c05a425284614
josephptflanagan <josephptflanagan@gmail.com>
2020-07-03 10:56:01 -0500

    getting started

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,71 @@
 console.log("Hello");
 
-function vimeoData(){
+function getData(searchTerm){ 
 
+    //COCKTAIL
+    var cocktailData = null;
+
+    //CHUCK NORRIS
+    var chuckNorrisData = null;
+
+    //GIPHY
+    var giphyData = null;
+
+    //WIKIPEDIA
+    var wikipediaData = null;
+
+    //CURRENT
+    var currentData = null;
+
+    //VIMEO 
+    var vimeoData = null;
     var videoId = "172825105";
-    fetch("https://api.vimeo.com/videos/" + videoId)
+    var vimeoUrl = "https://api.vimeo.com/videos/" + videoId
+
+    
+    fetch(vimeoUrl)
+    .then(function(response){
+        if(response.ok){
+            response.json().then
+        }
+    })
+
+    compileData();
+
 }
+
+function compileData(){
+
+}
+
+//when the random is clicked
+$("#random-button").on("click", function(){
+
+    //get and store the searchTerm from the search bar
+    var searchTerm = $(this).siblings("#search-bar").val().trim();
+    
+    if(searchTerm != ""){
+        //sends viable searchTerm to the getData function
+        getData(searchTerm);
+    }
+    else{
+        return;
+    }
+
+});
+
+//when the search bar is clicked
+$("#search-button").on("click", function(){
+
+    //get and store the searchTerm from the search bar
+    var searchTerm = $(this).siblings("#search-bar").val().trim();
+    
+    if(searchTerm != ""){
+        //sends viable searchTerm to the getData function
+        getData(searchTerm);
+    }
+    else{
+        return;
+    }
+
+});

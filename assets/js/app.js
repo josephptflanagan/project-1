517c4d674bebda041101f9f0df8d43ccbe30493d
Claudia Lay <claudialay@claudias-mbp.home>
2020-07-05 19:38:39 -0500

    added current api to develop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,128 +1,4 @@
-var button = document.querySelector("#random-click");
-var domEl = document.querySelector("#current-news");
 
-// click event to pull current news
-var submitRandom = function (event) 
-{
-    currentNews();
-}
-
-// API request from current news function
-var currentNews = function () 
-{
-    var apiURL = "https://api.currentsapi.services/v1/latest-news?english=us&apiKey=pqzZJrIzA7wKGaeyM4YYpxXkqkA6mmvHesnowVpytmqiX-0d";
-
-    fetch(apiURL).then(function (response) 
-    {
-        if (response.ok) 
-        {
-            response.json().then(function (data) 
-            {
-                displayNews(data);
-            })
-        }
-    });
-
-}
-
-var displayNews = function (data) 
-{
-    domEl.innerHTML = "";
-    // length of article array
-    var numArticles = data.news.length
-    // console.log(numArticles);
-
-    for (i = 0; i < 3; i++) 
-    {
-        // function to choose random number from array
-        var randomNumber = (Math.floor(Math.random() * numArticles))
-
-        // pull random article name
-        var articleTitle = data.news[randomNumber].title;
-
-        // pull description of article
-        var articleDescrip = data.news[randomNumber].description;
-
-        // pull article's image
-        var articleImg = data.news[randomNumber].image;
-
-        // pull article's link for the website
-        var articleUrl = data.news[randomNumber].url;
-
-        // create unordered list element
-        var containerEl = document.createElement("ul");
-        containerEl.classList = "cards"
-
-        // create list element for unordered list
-        var listEl = document.createElement("li");
-        listEl.classList= "cards_item";
-        containerEl.appendChild(listEl);
-
-        // create div element for every card
-        var divEl = document.createElement("div");
-        divEl.classList= "card"
-
-        if (articleImg === "None") 
-        {
-            // if no image available for the article append bored at home img
-            // var boredImg = document.createElement("img");
-            // boredImg.setAttribute("src", "./assets/image.png")
-            // boredImg.classList = "card_img"
-            // divEl.appendChild(boredImg);
-            
-        }
-        else 
-        {
-            // create link for image to article
-            var imgLinkEl = document.createElement("a");
-            imgLinkEl.classList = "card_image"
-            imgLinkEl.setAttribute("href", articleUrl);
-            imgLinkEl.setAttribute("target", "_blank");
-
-            // pull image url and create image element
-            var imgEl = document.createElement("img");
-            imgEl.setAttribute("src", articleImg);
-
-            // append link to image
-            imgLinkEl.appendChild(imgEl);
-            
-            // append to the div element with class card
-            divEl.appendChild(imgLinkEl);
-        }
-
-        
-
-        // create link element to link title
-        var titleEl = document.createElement("a");
-        titleEl.classList = "card_title";
-        titleEl.setAttribute("href", articleUrl);
-        titleEl.setAttribute("target", "_blank")
-        titleEl.textContent = articleTitle;
-        divEl.appendChild(titleEl);
-
-        // create p element for article description
-        var descripEl = document.createElement("p");
-        descripEl.classList = "card_text";
-        descripEl.textContent = articleDescrip;
-
-        // append title and description to content element
-        contentEl.appendChild(titleEl);
-        contentEl.appendChild(descripEl);
-
-        // append content element to div el (each article card)
-        divEl.appendChild(contentEl);
-
-        // append div element to list element
-        listEl.appendChild(divEl);
- 
-        // append to the dom
-        domEl.appendChild(containerEl);
-    }
-
-}
-
-button.addEventListener('click', submitRandom);
------
 var drinkButton = document.getElementById("drink-button");
 var displayCurrentEl = document.querySelector("#section-0");
 var displayChuckEl = document.querySelector("#section-1");
@@ -141,8 +17,8 @@ var getData = async (searchTerm) => {
     if (searchTerm == "") {
 
         //CURRENT
-        //var currentUrl = "";
-        //urlArray.push(currentUrl);
+        var currentUrl = "https://api.currentsapi.services/v1/latest-news?english=us&apiKey=pqzZJrIzA7wKGaeyM4YYpxXkqkA6mmvHesnowVpytmqiX-0d";
+        urlArray.push(currentUrl);
 
         //CHUCK NORRIS
         //var chuckNorrisUrl = "";
@@ -190,20 +66,101 @@ function displayData(dataArray) {
     $("#section-1").empty();
     $("#section-2").empty();
 
-    /*
+    
     //RANDOM DATA START
-    if(dataArray.length == 1){//ALERT ALERT ALERT ALERT ALERT <--------------------THIS "1" IS ONLY FOR TESTING PURPOSES, SHOULD BE 4 FOR RANDOM WHEN DATA IS AVAILABLE
+    if(dataArray.length == 2){//ALERT ALERT ALERT ALERT ALERT <--------------------THIS "1" IS ONLY FOR TESTING PURPOSES, SHOULD BE 4 FOR RANDOM WHEN DATA IS AVAILABLE
         
         //update buttons
         displayButtons("random")
 
         //CURRENT DISPLAY START dataArray[0]
-        displayCurrentEl.innerHTML = `
-        <div class="pure-g">
-            <div class="pure-u-3-5" id="news">
-                NEWS!!!!!  Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quam voluptates sit fugit, similique, dolores quae sint corrupti, dolore est doloribus minima officia voluptatibus! Doloribus adipisci expedita placeat repellat tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tenetur cupiditate adipisci nostrum beatae. Rem nesciunt unde quasi dolore cumque, labore est animi, recusandae beatae doloremque fugit eaque, vero iure.
-            </div>
-        </div>`;
+
+    var numArticles = dataArray[0].news.length
+    // console.log(numArticles);
+
+    for (i = 0; i < 3; i++) 
+    {
+        // function to choose random number from array
+        var randomNumber = (Math.floor(Math.random() * numArticles))
+
+        // pull random article name
+        var articleTitle = dataArray[0].news[randomNumber].title;
+
+        // pull description of article
+        var articleDescrip = dataArray[0].news[randomNumber].description;
+
+        // pull article's image
+        var articleImg = dataArray[0].news[randomNumber].image;
+
+        // pull article's link for the website
+        var articleUrl = dataArray[0].news[randomNumber].url;
+
+        // create unordered list element
+        var containerEl = document.createElement("ul");
+        containerEl.classList = "cards"
+
+        // create list element for unordered list
+        var listEl = document.createElement("li");
+        listEl.classList= "cards_item";
+        containerEl.appendChild(listEl);
+
+        // create div element for every card
+        var divEl = document.createElement("div");
+        divEl.classList= "card"
+
+        if (articleImg === "None") 
+        {
+          
+        }
+        else 
+        {
+            // create link for image to article
+            var imgLinkEl = document.createElement("a");
+            imgLinkEl.classList = "card_image"
+            imgLinkEl.setAttribute("href", articleUrl);
+            imgLinkEl.setAttribute("target", "_blank");
+
+            // pull image url and create image element
+            var imgEl = document.createElement("img");
+            imgEl.setAttribute("src", articleImg);
+
+            // append link to image
+            imgLinkEl.appendChild(imgEl);
+            
+            // append to the div element with class card
+            divEl.appendChild(imgLinkEl);
+        }
+
+        // create link element to link title
+        var titleEl = document.createElement("a");
+        titleEl.classList = "card_title";
+        titleEl.setAttribute("href", articleUrl);
+        titleEl.setAttribute("target", "_blank")
+        titleEl.textContent = articleTitle;
+        divEl.appendChild(titleEl);
+
+        // create p element for article description
+        var descripEl = document.createElement("p");
+        descripEl.classList = "card_text";
+        descripEl.textContent = articleDescrip;
+
+        // append title and description to content element
+        var contentEl = document.createElement('div');
+        contentEl.classList ='card_content';
+        contentEl.appendChild(titleEl);
+        contentEl.appendChild(descripEl);
+
+        // append content element to div el (each article card)
+        divEl.appendChild(contentEl);
+
+        // append div element to list element
+        listEl.appendChild(divEl);
+ 
+        // append to the dom
+        displayCurrentEl.appendChild(containerEl);
+    }
+
+
         //CURRENT DISPLAY END
 
         //CHUCK NORRIS DISPLAY START dataArray[1] (CONTAINS GIPHY DISPLAY dataArray[2])
@@ -218,106 +175,106 @@ function displayData(dataArray) {
 
         
         //COCKTAIL DISPLAY START dataArray[0] for now, dataArray[3] once all are present
-        displayCocktailEl.innerHTML = `
-        <div>
-            <h2 id=“cocktailName”>${dataArray[0].drinks[0].strDrink}</h2>
-            <img src=${dataArray[0].drinks[0].strDrinkThumb} alt=${dataArray[0].strDrink} id=“cocktailImage”/>
-        </div>
-        <div>
-            <p id=“cocktailInstructions”>${dataArray[0].drinks[0].strInstructions}</p>
-        </div>
-        <h3>Ingredients</h3>
-        <span id=“measure1">${dataArray[0].drinks[0].strMeasure1}</span>
-        <span id=“ingredient1”>${dataArray[0].drinks[0].strIngredient1}</span>`;
-        if (dataArray[0].drinks[0].strIngredient2 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement2">${dataArray[0].drinks[0].strMeasure2}</span>
-            <span id=“ingredient2”>${dataArray[0].drinks[0].strIngredient2}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient3 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement3">${dataArray[0].drinks[0].strMeasure3}</span>
-            <span id=“ingredient3”>${dataArray[0].drinks[0].strIngredient3}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient4 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement4">${dataArray[0].drinks[0].strMeasure4}</span>
-            <span id=“ingredient4”>${dataArray[0].drinks[0].strIngredient4}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient5 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement5">${dataArray[0].drinks[0].strMeasure5}</span>
-            <span id=“ingredient5”>${dataArray[0].drinks[0].strIngredient5}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient6 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement6">${dataArray[0].drinks[0].strMeasure6}</span>
-            <span id=“ingredient6”>${dataArray[0].drinks[0].strIngredient6}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient7 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement7">${dataArray[0].drinks[0].strMeasure7}</span>
-            <span id=“ingredient7”>${dataArray[0].drinks[0].strIngredient7}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient8 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement8">${dataArray[0].drinks[0].strMeasure8}</span>
-            <span id=“ingredient8”>${dataArray[0].drinks[0].strIngredient8}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient9 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement9">${dataArray[0].drinks[0].strMeasure9}</span>
-            <span id=“ingredient9”>${dataArray[0].drinks[0].strIngredient9}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient10 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement10">${dataArray[0].drinks[0].strMeasure10}</span>
-            <span id=“ingredient10”>${dataArray[0].drinks[0].strIngredient10}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient11 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement11">${dataArray[0].drinks[0].strMeasure11}</span>
-            <span id=“ingredient11”>${dataArray[0].drinks[0].strIngredient11}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient12 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement12">${dataArray[0].drinks[0].strMeasure12}</span>
-            <span id=“ingredient12”>${dataArray[0].drinks[0].strIngredient12}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient13 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement13">${dataArray[0].drinks[0].strMeasure13}</span>
-            <span id=“ingredient13”>${dataArray[0].drinks[0].strIngredient13}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient14 !== null) {
-            displayCocktailEl.innerHTML += `
-            <span id=“measurement14">${dataArray[0].drinks[0].strMeasure14}</span>
-            <span id=“ingredient14”>${dataArray[0].drinks[0].strIngredient14}</span>
-            `;
-        }
-        if (dataArray[0].drinks[0].strIngredient15 !== null) {
-            displayCocktailEl.innerHTML += `
-                <span id=“measurement15">${dataArray[0].drinks[0].strMeasure15}</span>
-                <span id=“ingredient15”>${dataArray[0].drinks[0].strIngredient15}</span>
-            `;
-        }
+        // displayCocktailEl.innerHTML = `
+        // <div>
+        //     <h2 id=“cocktailName”>${dataArray[0].drinks[0].strDrink}</h2>
+        //     <img src=${dataArray[0].drinks[0].strDrinkThumb} alt=${dataArray[0].strDrink} id=“cocktailImage”/>
+        // </div>
+        // <div>
+        //     <p id=“cocktailInstructions”>${dataArray[0].drinks[0].strInstructions}</p>
+        // </div>
+        // <h3>Ingredients</h3>
+        // <span id=“measure1">${dataArray[0].drinks[0].strMeasure1}</span>
+        // <span id=“ingredient1”>${dataArray[0].drinks[0].strIngredient1}</span>`;
+        // if (dataArray[0].drinks[0].strIngredient2 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement2">${dataArray[0].drinks[0].strMeasure2}</span>
+        //     <span id=“ingredient2”>${dataArray[0].drinks[0].strIngredient2}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient3 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement3">${dataArray[0].drinks[0].strMeasure3}</span>
+        //     <span id=“ingredient3”>${dataArray[0].drinks[0].strIngredient3}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient4 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement4">${dataArray[0].drinks[0].strMeasure4}</span>
+        //     <span id=“ingredient4”>${dataArray[0].drinks[0].strIngredient4}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient5 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement5">${dataArray[0].drinks[0].strMeasure5}</span>
+        //     <span id=“ingredient5”>${dataArray[0].drinks[0].strIngredient5}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient6 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement6">${dataArray[0].drinks[0].strMeasure6}</span>
+        //     <span id=“ingredient6”>${dataArray[0].drinks[0].strIngredient6}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient7 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement7">${dataArray[0].drinks[0].strMeasure7}</span>
+        //     <span id=“ingredient7”>${dataArray[0].drinks[0].strIngredient7}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient8 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement8">${dataArray[0].drinks[0].strMeasure8}</span>
+        //     <span id=“ingredient8”>${dataArray[0].drinks[0].strIngredient8}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient9 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement9">${dataArray[0].drinks[0].strMeasure9}</span>
+        //     <span id=“ingredient9”>${dataArray[0].drinks[0].strIngredient9}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient10 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement10">${dataArray[0].drinks[0].strMeasure10}</span>
+        //     <span id=“ingredient10”>${dataArray[0].drinks[0].strIngredient10}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient11 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement11">${dataArray[0].drinks[0].strMeasure11}</span>
+        //     <span id=“ingredient11”>${dataArray[0].drinks[0].strIngredient11}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient12 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement12">${dataArray[0].drinks[0].strMeasure12}</span>
+        //     <span id=“ingredient12”>${dataArray[0].drinks[0].strIngredient12}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient13 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement13">${dataArray[0].drinks[0].strMeasure13}</span>
+        //     <span id=“ingredient13”>${dataArray[0].drinks[0].strIngredient13}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient14 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //     <span id=“measurement14">${dataArray[0].drinks[0].strMeasure14}</span>
+        //     <span id=“ingredient14”>${dataArray[0].drinks[0].strIngredient14}</span>
+        //     `;
+        // }
+        // if (dataArray[0].drinks[0].strIngredient15 !== null) {
+        //     displayCocktailEl.innerHTML += `
+        //         <span id=“measurement15">${dataArray[0].drinks[0].strMeasure15}</span>
+        //         <span id=“ingredient15”>${dataArray[0].drinks[0].strIngredient15}</span>
+        //     `;
+        // }
         //COCKTAIL DISPLAY END
         
         //RANDOM DATA END
     }
-       */
+       
     if (dataArray.length == 1) {//SEARCH DATA START temporarily set to 1 to test vimeo, reset to 2, restore else if
 
         //update buttons
